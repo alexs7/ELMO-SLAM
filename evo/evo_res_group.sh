195082137c5b7ecaba24498edd918ea972a871b9
ziwen <dkaka950316@gmail.com>
2022-04-29 16:09:41 +0100

    bulk processing scripts for kitti to get result file for evo_res

--- evo/evo_res_group.sh
+++ evo/evo_res_group.sh
@@ -12,39 +12,65 @@ label=${2:-default}
 
 sequence=${3:-00}
 
+sequence_pattern="*$sequence*"
+
 pattern="*$label*"
 
-echo $look_up_dir
-echo $label
-echo $sequence
-echo $pattern
+echo "$look_up_dir"
+echo "$label"
 
-lines=$(find . -type d -name $pattern | wc -l)
-echo $lines
-if test $lines -eq 0
+echo "$sequence"
+echo "$pattern"
+
+
+lines=$(find "$look_up_dir" -type d -name  "$pattern" | wc -l)
+echo find "$look_up_dir" -type d -name  "$pattern"
+echo "$lines"
+if test "$lines" -eq 0
 then
   echo "Cannot find directory with the given keyword, check argument 1 and 2"
-
-elif test $lines -eq 1
+  exit 0
+elif test "$lines" -eq 1
   then
-  echo "find single directory of $(find . -type d -name $pattern )"
+  echo find single directory of "$(find "$look_up_dir" -type d -name "$pattern")"
+#  echo $(basename $(find $look_up_dir -type d -name $pattern))
 
 else
-   echo "find more than one directory as below, try again with more specific keyword"
-   echo  $(find . -type d -name $pattern )
+   echo "find more than one directory as below, try again with more specific keyword:"
+   echo  "$(find "$look_up_dir" -type d -name "$pattern")"
    exit 0
 fi
 
-#
-#
-#if [ -d "$ARG1"-"$(date +"%d-%m-%Y")" ]
-#then
-#    echo "Locating result file in  /path/to/dir exists. skip creating"
-#else
-#    echo "Error: Directory does not exists, creating"
-#    echo mkdir "$ARG1"-"$(date +"%d-%m-%Y")"
-#    mkdir "$ARG1"-"$(date +"%d-%m-%Y")"
-#fi
+found_dir=$(find "$look_up_dir" -type d -name "$pattern")
+echo " $found_dir"
+lines=$(find "$look_up_dir" -name "$pattern" | wc -l)
+echo "find $lines: at $found_dir"
+echo $(find "$found_dir" -name "$sequence_pattern")
 
 
-#mkdir "$ARG1"-$(date +"%d-%m-%Y")
+result_folder="$(basename $(find "$look_up_dir" -type d -name "$pattern"))"_result
+echo  "$result_folder"
+
+if [ -d "$result_folder" ]
+then
+    echo "Result folder $result_folder exists. skip creating and processing result text file"
+else
+    echo "Make directory for containign result file with name $result_folder"
+    mkdir "$result_folder"
+
+    IFS=$'\n'
+    for i in $(find "$found_dir" -name "$sequence_pattern");
+    do
+#        echo "$i"
+        evo_ape tum /home/ziwen/elmo_ws/ELMO-SLAM/evo/test_set/data_odometry_poses/dataset/poses/kitt_"$sequence"_tum.txt "$i" -as --save_results "$result_folder"/$(basename $(dirname "$i"))_"$sequence"_result.zip
+    #     --plot  --plot_mode xz
+    done
+    unset IFS
+
+fi
+
+
+
+evo_res "$result_folder"/*.zip -p --use_filenames
+
+
